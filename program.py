5a846519e7e6b56d700bea59f65e94557fc61826
kopperqu <email@email.com>
2023-04-06 11:41:06 -0500

    Further work

--- program.py
+++ program.py
@@ -19,53 +19,75 @@ def getAthletesNamesAndTffrsLinks(url):
         tffrsLink.append('https://www.tfrrs.org'+eachLink['href'])
     return(names,tffrsLink)
 
-mensTrackURL="https://www.tfrrs.org/teams/tf/WI_college_m_Wis_Stevens_Point.html"
-html = urlopen(mensTrackURL)
-soup=BeautifulSoup(html.read(), "html.parser")
-womensTrackURL = 'https://www.tfrrs.org'+soup.find('a',text='Women\'s Track & Field')['href']
+# mensTrackURL="https://www.tfrrs.org/teams/tf/WI_college_m_Wis_Stevens_Point.html"
+# html = urlopen(mensTrackURL)
+# soup=BeautifulSoup(html.read(), "html.parser")
+# womensTrackURL = 'https://www.tfrrs.org'+soup.find('a',text='Women\'s Track & Field')['href']
 
-men,menTffrsLink = getAthletesNamesAndTffrsLinks(mensTrackURL)
-women,womenTffrsLink = getAthletesNamesAndTffrsLinks(womensTrackURL)
-names=men+women
-tffrsLinks=menTffrsLink+womenTffrsLink
+# men,menTffrsLink = getAthletesNamesAndTffrsLinks(mensTrackURL)
+# women,womenTffrsLink = getAthletesNamesAndTffrsLinks(womensTrackURL)
+# names=men+women
+# tffrsLinks=menTffrsLink+womenTffrsLink
+# throwers=[]
+# throwersEvents=[]
+# throwersMarks=[]
+# for index,eachPerson in enumerate(names):
+#     html = urlopen(tffrsLinks[index])
+#     soup=BeautifulSoup(html.read(), "html.parser")
+#     bestsRows=soup.find('table',id='all_bests').findAll('td')
+#     events=[]
+#     marks=[]
+#     for index,each in enumerate(bestsRows):
+#         if index%2==0:
+#             events.append(each.getText().strip())
+#         else:
+#             marks.append(each.getText().strip())
+#     isThrower=False
+#     newMarks = []
+#     newEvents = []
+#     for eventIndex,eachEvent in enumerate(events):        
+#         if eachEvent in ['SP','WT','DT','HT','JT']:
+#             if(not isThrower):
+#                 isThrower=True
+#                 throwers.append(eachPerson)
+#             holdCharacters=[]
+#             tempmark=''
+#             #Need to go through mark and find m then only use what before m for the mark.
+#             for eachLetter in marks[eventIndex]:
+#                 if eachLetter=='m':
+#                     break
+#                 holdCharacters.append(eachLetter)
+#             for each in holdCharacters:
+#                 tempmark=tempmark+each
+#             newMarks.append(tempmark)
+#             newEvents.append(eachEvent)
+#     if(isThrower):
+#         throwersEvents.append(newEvents)
+#         throwersMarks.append(newMarks)
+
+#Saving results from running the above code so I
+#I can work on this offline if necessary and I no longer need to 
+#grab all the html everytime I want to work on this.
+#This can be remove when implementing the final version so it 
+#gets most recent results
+#forjson = zip(throwers,throwersEvents,throwersMarks)
+with open('throwers.txt','r') as f:
+    data = f.read().split('\n')
+
 throwers=[]
 throwersEvents=[]
 throwersMarks=[]
-for index,eachPerson in enumerate(names):
-    html = urlopen(tffrsLinks[index])
-    soup=BeautifulSoup(html.read(), "html.parser")
-    bestsRows=soup.find('table',id='all_bests').findAll('td')
-    events=[]
-    marks=[]
-    for index,each in enumerate(bestsRows):
-        if index%2==0:
-            events.append(each.getText().strip())
-        else:
-            marks.append(each.getText().strip())
-    isThrower=False
-    newMarks = []
-    newEvents = []
-    for eventIndex,eachEvent in enumerate(events):        
-        if eachEvent in ['SP','WT','DT','HT','JT']:
-            if(not isThrower):
-                isThrower=True
-                throwers.append(eachPerson)
-            holdCharacters=[]
-            tempmark=''
-            #Need to go through mark and find m then only use what before m for the mark.
-            for eachLetter in marks[eventIndex]:
-                if eachLetter=='m':
-                    break
-                holdCharacters.append(eachLetter)
-            for each in holdCharacters:
-                tempmark=tempmark+each
-            newMarks.append(tempmark)
-            newEvents.append(eachEvent)
-    if(isThrower):
-        throwersEvents.append(newEvents)
-        throwersMarks.append(newMarks)
+for index,each in enumerate(data):
+    if index%3==0:
+        throwers.append(data[index].strip())
+    if index%3==1:  
+        throwersEvents.append(data[index].strip().split(' '))
+    if index%3==2:
+        throwersMarks.append(data[index].strip().split(' '))
+for index,eachThrower in enumerate(throwers):
+    print(eachThrower,throwersEvents[index],throwersMarks[index])
+
 #At this point we only have people who have thrown a throwing event so we 
 #can assume we have all throwers unless they have not thrown in an event.
 #the above code should be ran every day at midnight
-for index,eachThrower in enumerate(throwers):
-    print(eachThrower,throwersEvents[index],throwersMarks[index])
+
